e121e34fcaae9620d422d9b42d31cf083fc28a21
Stuart Leitch <toruitas@gmail.com>
2020-02-07 12:04:33 +0000

    Make initial file

---
+++ scrape_corona.py
@@ -0,0 +1,36 @@
+from gensim.summarization import summarize
+import pandas
+import numpy as np
+import bs4
+import requests
+
+
+def get_link_response(url:str) -> requests.Response:
+    """
+    get_link_response gets a URL and scrapes it, returning the object.
+    """
+    return requests.get(url)
+
+
+def choose_next_link(urls: list) ->list:
+    """
+    choose_next_link
+
+    Given a list of URLs strings from the website scraped, it chooses which link to go to next.
+    """
+    url_keywords = ["breitbart", "foxnews", "thehill", "dailymail", "wallstreet", "drudgereport", "hannity", "trump"]
+    next_links = []
+    for link in urls:
+        for rightist_link in url_keywords:
+            if rightist_link in link:
+                next_links.append(link)
+
+    return next_links
+
+
+
+if __name__ == "__main__":
+    print("Scraping stuff for corona. Limes!")
+
+    while True:
+        
